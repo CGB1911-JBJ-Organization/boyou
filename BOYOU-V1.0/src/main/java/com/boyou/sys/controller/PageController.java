c08099233a36203afdd22d792c9a6df7d9cb6bd3
蒋伯军 <801@801-PC>
2020-02-22 18:09:24 +0800

    666

---
+++ BOYOU-V1.0/src/main/java/com/boyou/sys/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.boyou.sys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 此Controller主要负责响应一些页面
+ */
+@Controller
+@RequestMapping("/")
+public class PageController {
+
+}
