3cc2a4ea38c308eb3ec02e670bd11790f8cfb78e
蒋伯军 <801@801-PC>
2020-02-22 09:35:08 +0800

    1

--- BOYOU-V1.0/src/main/java/com/boyou/Application.java
+++ BOYOU-V1.0/src/main/java/com/boyou/Application.java
@@ -3,7 +3,7 @@ package com.boyou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//
 @SpringBootApplication
 public class Application {
 
