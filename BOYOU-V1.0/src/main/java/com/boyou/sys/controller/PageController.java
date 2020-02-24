95dd8909f7b41c292688304e536cee8cf3e3155f
LEF <LEF@DESKTOP-UMNDL4Q>
2020-02-24 21:27:20 +0800

    此次提交网页头部尾部及其js和css，和项目初始控制类

--- BOYOU-V1.0/src/main/java/com/boyou/sys/controller/PageController.java
+++ BOYOU-V1.0/src/main/java/com/boyou/sys/controller/PageController.java
@@ -1,13 +1,44 @@
 package com.boyou.sys.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 /**
- * 此Controller主要负责响应一些页面
+ * 此Controller主要负责响应页面
  */
 @Controller
 @RequestMapping("/")
 public class PageController {
-
+	
+	/**
+	 * 此方法用于响应网站首页
+	 * @return 首页路径
+	 */
+	@RequestMapping("boyou")
+	public String startPage() {
+		return "";
+	}
+	
+	/**
+	 * 此方法响应页面的请求
+	 * @param pageName 页面名称
+	 * @return 页面路径
+	 */
+	@RequestMapping("doPageUi/{pageName}")
+	public String doPageUi(@PathVariable String pageName) {
+		return "sys/"+pageName;
+	}
+	
+	/**
+	 * 此方法响应通用页面模块
+	 * @param mudule 页面模块名称
+	 * @return 模块文件路径
+	 */
+	@RequestMapping("doPageMudule/{module}")
+	public String doPageMudule(@PathVariable String mudule) {
+		return "common/"+mudule;
+	}
+	
 }
---
+++ BOYOU-V1.0/src/main/resources/static/dist/js/travelSquare.js
@@ -0,0 +1,26 @@
+
+/*文档事件就绪函数*/
+$(function(){
+	doLoadUi("header","/doPageMudule/header");
+	doLoadUi("footer","/doPageMudule/footer");
+})
+
+/*加载网页元素函数*/
+function doLoadUi(id,url){
+	$("#"+id).click(function(){
+		$("#mainContentId").load(url);
+	});
+}
+
+/* 点击更多打开筛选输入框时间事件函数 */
+function openPayLine2(moreButton){
+    /* console.log("开始"); */
+    var status = $(moreButton).text();
+    if (status == "更多>") {
+       $(moreButton).text("收起<");
+	}else{
+       $(moreButton).text("更多>");
+	}
+    $(moreButton).parent().next().toggle(120);
+}
+
