d64aacc89ec01df583fff7a17e6ad5abc3147278
LH <LH@192.168.0.104>
2020-02-29 21:03:21 +0800

    改掉了一些BUG

--- src/main/java/com/cy/boyou/sys/controller/UserController.java
+++ src/main/java/com/cy/boyou/sys/controller/UserController.java
@@ -18,14 +18,11 @@ public class UserController {
 		// 1.获取Subject对象
 		Subject subject = SecurityUtils.getSubject();
 		// 2.通过Subject提交用户信息,交给shiro框架进行认证操作
-		// 2.1对用户进行封装
+		// 对用户进行封装
 		UsernamePasswordToken token = new UsernamePasswordToken(username, // 身份信息
 				password);// 凭证信息
-		// 2.2对用户信息进行身份认证
+		// 对用户信息进行身份认证
 		subject.login(token);
-		// 1)token会传给shiro的SecurityManager
-		// 2)SecurityManager将token传递给认证管理器
-		// 3)认证管理器会将token传递给realm
 		return new JsonResult("登录成功！");
 	}
 }
