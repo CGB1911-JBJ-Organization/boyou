b28095aa5b39cab2ddeea2bb7207130c17f83941
LH <LH@192.168.0.104>
2020-02-29 21:45:42 +0800

    修改了User类

--- src/main/java/com/cy/boyou/sys/entity/User.java
+++ src/main/java/com/cy/boyou/sys/entity/User.java
@@ -12,18 +12,20 @@ import lombok.ToString;
 @ToString
 public class User implements Serializable{
 		private static final long serialVersionUID = 5053909074079096841L;
-		private Integer userId;
-		private String userAccount;
-		private String userPwd;
-		private String userName;
+		private Integer userId;//id
+		private String userAccount;//用户名
+		private String userPwd;//密码
 		private String userSalt;//盐值
-		private String userEmail;
-		private String userAddress;
+		private String userName;//昵称
+		private Date birthday;//生日
+		private Integer userType;//用户类型
 		private Integer noteNumber;//游记数量
-		private Integer travelNumber;
-		private Date birthday;
+		private Integer tNumber;//游记数量
+		private String userSex;//用户性别
+		private String userAddress;
+		private String userEmail;
 		private String headImg;//头像地址
-		private String userSex;
+		private Integer travelNumber;//景区ID
 
 	
 }
